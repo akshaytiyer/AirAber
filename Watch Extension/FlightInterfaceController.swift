19cb737dd716ca9aa0550b5787840cf285c683c1
Akshay Iyer <akshay.iyer@nyu.edu>
2017-03-26 14:11:52 -0400

    Initial Commit and Connected Outlets

---
+++ Watch Extension/FlightInterfaceController.swift
@@ -0,0 +1,47 @@
+//
+//  FlightInterfaceController.swift
+//  AirAber
+//
+//  Created by Akshay Iyer on 3/26/17.
+//  Copyright © 2017 Mic Pringle. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class FlightInterfaceController: WKInterfaceController {
+    
+    @IBOutlet var flightLabel: WKInterfaceLabel!
+    @IBOutlet var routeLabel: WKInterfaceLabel!
+    @IBOutlet var boardingLabel: WKInterfaceLabel!
+    @IBOutlet var boardTimeLabel: WKInterfaceLabel!
+    @IBOutlet var statusLabel: WKInterfaceLabel!
+    @IBOutlet var gateLabel: WKInterfaceLabel!
+    @IBOutlet var seatLabel: WKInterfaceLabel!
+    
+    // 1
+    var flight: Flight? {
+        // 2
+        didSet {
+            // 3
+            if let flight = flight {
+                // 4
+                flightLabel.setText("Flight \(flight.shortNumber)")
+                routeLabel.setText(flight.route)
+                boardingLabel.setText("\(flight.number) Boards")
+                boardTimeLabel.setText(flight.boardsAt)
+                // 5
+                if flight.onSchedule {
+                    statusLabel.setText("On Time")
+                } else {
+                    statusLabel.setText("Delayed")
+                    statusLabel.setTextColor(UIColor.red)
+                }
+                gateLabel.setText("Gate \(flight.gate)")
+                seatLabel.setText("Seat \(flight.seat)")
+            }
+        }
+    }
+        
+}
